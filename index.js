5a2ee3be5e9c12c21f4b1abe2e3c571246fdcc30
Tushar Kumar <tusharkumar@192.168.1.16>
2023-04-08 14:23:21 +0530

    [NEW] changed the origin port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = process.env.port || 4200;
+const port = process.env.port || 3000;
 const puppeteer = require("puppeteer");
 const cors = require("cors");
 const pdfTemp = require("./routes/pdf");
